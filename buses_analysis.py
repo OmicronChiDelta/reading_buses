9bac5897f663f0f64c5bb0d964f6dba494f7ca0d
Alex <50920163+OmicronChiDelta@users.noreply.github.com>
2020-04-29 15:20:15 +0100

    First commit
    Setting up anaconda environments and core files

---
+++ buses_analysis.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Various pieces of analysis for the Reading Buses datasets
+
+@author: Alex White
+"""
+
---
+++ buses_utils.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Functions to assist in analysing Reading Buses open source datasets
+
+@author: Alex White
+"""
+
