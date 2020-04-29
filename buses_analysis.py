21ba285af5f2db88bb82dc93e6c2aa03aafe4a02
Alex <50920163+OmicronChiDelta@users.noreply.github.com>
2020-04-29 17:40:38 +0100

    bus network geometry inspection
    Created a function to return key info on the bus network - lat/long of stations, routes etc.

--- buses_analysis.py
+++ buses_analysis.py
@@ -4,4 +4,18 @@ Various pieces of analysis for the Reading Buses datasets
 
 @author: Alex White
 """
+import sys
+
+utils_dir = 'C:\\Users\\Alex White\\Documents\GitHub\reading_buses'
+if utils_dir not in sys.path:
+    sys.path.append(utils_dir)
+
+from buses_utils import recover_geometry
+
+api_key = 'D24LEmypWC'
+url_geometry = 'https://rtl2.ods-live.co.uk//api/busstops?key={}'.format(api_key)
+
+#Obtain network geometry        
+df_geometry = recover_geometry(url_geometry)
+
 
--- buses_utils.py
+++ buses_utils.py
@@ -5,3 +5,42 @@ Functions to assist in analysing Reading Buses open source datasets
 @author: Alex White
 """
 
+import pandas as pd
+import urllib
+import re
+import numpy as np
+
+def recover_geometry(url_geometry):
+    '''
+    Function to parse the network geometry dumped via the API.
+    Input:
+        url_geometry: string specifying the url to open, containing the geometry info
+    Returns:
+        df_geometry: a pandas dataframe containing various fields, including the Lat/Long
+        of stops, the route to which they belong etc. 
+    '''
+    
+    dump_geometry = urllib.request.urlopen(url_geometry).read()
+    string_geometry = dump_geometry.decode('utf-8')
+
+    regex_record = '\{(.*?)\},'
+    records = re.findall(regex_record, string_geometry)
+    
+    regex_field = '^.*?:|,.*?:'
+    regex_data  = ':.*?,|:.*?$'
+    
+    #One-off inspection to get the fields:
+    fields = re.findall(regex_field, records[0])
+    fields = [f.replace(':', '').replace('"', '').replace(',', '') for f in fields]
+    
+    #Set up space to save time
+    df_geometry = pd.DataFrame(columns=fields, data=[['' for i in fields] for j in records])
+    
+    #Insert all data into frame
+    for i, r in enumerate(records):    
+        data  = re.findall(regex_data, r)
+        data  = [d.replace(':', '').replace('"', '').replace(',', '') for d in data]
+    
+        for j, f in enumerate(fields):
+            df_geometry.at[i, f] = data[j] 
+    return df_geometry
